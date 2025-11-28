16e42f7172e2eb6344f4c65c14fbaf34c6d98aaf
Denys Batosevych <d.bartosevych@abtollc.com>
2025-11-28 17:42:27 +0200

    Initial commit

---
+++ src/lib/components/AppCard.svelte
@@ -0,0 +1,54 @@
+<script>
+    let { href, image } = $props()
+
+    let deg = $state(0);
+
+</script>
+
+
+<div class="cool-border col-span-1 rounded-lg sm:h-46 h-32 w-full cursor-pointer shadow-lg shadow-green-800 hover:scale-105 transition-all"v>
+    <div class="bg-green-500 h-full w-full rounded-sm">
+        <h1>{image}</h1>
+    </div>
+</div>
+
+<style>
+    @property --gradient-angle {
+        syntax: '<angle>';
+        initial-value: 0deg;
+        inherits: false;
+    }
+
+    .cool-border{
+        background-clip: padding-box;
+        border: 5px solid transparent;
+        position: relative;
+
+        &:before{
+            border-radius: var(--radius);
+
+            content: ' ';
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: linear-gradient(var(--gradient-angle),#fff,#000);
+            margin: -5px;
+
+            animation: spin-gradient 3s linear infinite;
+            z-index: -10;
+            animation-play-state: paused;
+        }
+    }
+    .cool-border:hover:before{
+        animation-play-state: running;
+        z-index: -10;
+
+    }
+
+    @keyframes spin-gradient {
+        0% { --gradient-angle: 0deg; }
+        100% { --gradient-angle: 360deg; }
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import AppCard from "$lib/components/AppCard.svelte";
+</script>
+
+<h1 class="text-9xl text-center m-16">jscd</h1>
+
+<div class="grid xl:grid-cols-2 sm:grid-cols-1 gap-16 xl:m-48 2xl:m-64 md:m-32 m-16 !mt-16 transition-all">
+    <AppCard image="1" href="/" />
+    <AppCard image="2" href="/" />
+    <AppCard image="3" href="/" />
+    <AppCard image="4" href="/" />
+</div>
