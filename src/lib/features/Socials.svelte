beefdf171985238f95fad9cc24fb5a9a9d854ae3
jscd <jisreserv@gmail.com>
2025-12-01 01:06:53 +0200

    email

--- src/lib/features/Socials.svelte
+++ src/lib/features/Socials.svelte
@@ -3,6 +3,7 @@
 
 </script>
 
+<a class="email text-sm m-2" href="mailto:me@jscd.uk" >me@jscd.uk</a>
 <div class="fixed bottom-0 right-0 m-3">
     <a href="https://github.com/kotakitamaru">
         <Icon.Github  class="github" size="24" />
@@ -10,13 +11,20 @@
 </div>
 
 <style>
-    :global(.github) {
-        color: #223322;
-        opacity: 0.5;
-        cursor: pointer;
-        transition: all 300ms;
+    .email {
+        position: fixed;
+        bottom: 0;
+        left: 50%;
     }
-    :global(.github:hover) {
+
+    :global(.github:hover), .email:hover {
         opacity: 1;
     }
+
+    :global(.github), .email {
+        color: #223322;
+        opacity: 0.5;
+        transition: all 300ms;
+    }
+
 </style>
